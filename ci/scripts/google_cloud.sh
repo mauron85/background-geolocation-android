61cf056a307c9d2e336abf1e34ad304639cc6966
Marian Hello <m.hello@gmail.com>
2019-08-29 20:58:35 +0200

    update circleci config

--- ci/scripts/google_cloud.sh
+++ ci/scripts/google_cloud.sh
@@ -47,6 +47,6 @@ gcloud config set project ${GCLOUD_PROJECT}
 gcloud firebase test android run \
   --type instrumentation \
   --app ~/android/ci/res/dummy.apk \
-  --test $(ls -dt ~/android/build/outputs/apk/*.apk | head -1) \
+  --test $(ls -dt ~/android/build/outputs/apk/androidTest/debug/*.apk | head -1) \
   --device model=Nexus6,version=25,locale=en,orientation=portrait  \
   --timeout 90s
